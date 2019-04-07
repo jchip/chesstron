3d8e861990d78917632335d9be1e1877b7a4d779
Joel Chen <joel123@gmail.com>
2019-04-06 20:11:22 -0700

    3

--- main.js
+++ main.js
@@ -8,78 +8,79 @@ app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow = null;
 
-function createWindow() {
-  const PROTOCOL = "file";
+function initialize() {
+  const gotTheLock = app.requestSingleInstanceLock();
 
-  electron.protocol.interceptFileProtocol(PROTOCOL, (request, callback) => {
-    // // Strip protocol
-    let url = request.url.substr(PROTOCOL.length + 1);
+  if (!gotTheLock) {
+    app.quit();
+    return;
+  }
 
-    // Build complete path for node require function
-    url = path.join(__dirname, url);
+  function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+      width: 1150,
+      height: 900,
+      title: "Chesstron",
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
 
-    // Replace backslashes by forward slashes (windows)
-    // url = url.replace(/\\/g, '/');
-    url = path.normalize(url);
+    // and load the index.html of the app.
+    // mainWindow.loadFile("index.html");
 
-    callback({ path: url });
-  });
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file",
+        slashes: true
+      })
+    );
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1150,
-    height: 900,
-    webPreferences: {
-      nodeIntegration: true
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on("closed", function() {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null;
+    });
+  }
+
+  app.on("second-instance", (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
     }
   });
 
-  // and load the index.html of the app.
-  // mainWindow.loadFile("index.html");
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on("ready", createWindow);
 
-  mainWindow.loadURL(
-    url.format({
-      pathname: "index.html",
-      protocol: PROTOCOL + ":",
-      slashes: true
-    })
-  );
+  // Quit when all windows are closed.
+  app.on("window-all-closed", function() {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on("closed", function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
+  app.on("activate", function() {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+      createWindow();
+    }
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
-
-// Quit when all windows are closed.
-app.on("window-all-closed", function() {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", function() {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+initialize();
