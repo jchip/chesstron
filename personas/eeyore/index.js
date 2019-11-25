ea919a79b1d9b7da00959667e10d2db704a5dbc5
Joel Chen <joel123@gmail.com>
2019-11-24 19:16:56 -0800

    adjust eeyore

--- personas/eeyore/index.js
+++ personas/eeyore/index.js
@@ -3,8 +3,8 @@
 const _ = require("lodash");
 const util = require("../../lib/util");
 
-const makePvMove = async (engine, id) => {
-  const depth = 10;
+const makePvMove = async (engine, id, inDepth) => {
+  const depth = inDepth || util.pickChance([4, 5, 7, 8, 9, 10, 57], [7, 6, 5, 4, 3, 2, 1]);
   console.log(id, "about to make multi pv move, depth", depth);
   const result = await engine.go({
     depth,
@@ -12,20 +12,40 @@ const makePvMove = async (engine, id) => {
   });
   if (result.info.length > 1) {
     const sortedPv = result.info
-      .filter(x => x.pv && x.score && x.depth >= depth)
+      // avoid moves that puts eeyore in significant disadvantage if possible
+      // .filter(x => x.pv && x.score && x.score.value > -80)
       .sort((a, b) => {
         return b.score.value - a.score.value;
       });
 
-    if (sortedPv.length > 1) {
-      let scores = [];
-      const chances = [5, 5, 5, 10, 10, 15, 20, 15, 10, 5];
-      const picked = util.pickChance(chances.slice(0, Math.min(chances.length, sortedPv.length)));
+    console.log(id, "moves", result.info, "sorted", sortedPv);
 
-      const pv = sortedPv[picked] || sortedPv[0];
-      result._bestmove = result.bestmove;
-      result.bestmove = pv.pv.split(" ")[0];
-      scores = sortedPv.slice(0, 10).map(x => x.score.value);
+    if (sortedPv.length > 1) {
+      let picked;
+      let pv;
+
+      let scores = [];
+      const firstPv = sortedPv[0];
+      const firstMove = firstPv.pv.split(" ")[0];
+      const nextPv = sortedPv.find(x => x.pv.split(" ")[0] !== firstMove);
+      // opponent most likely made a big blunder, take obvious move
+      if (nextPv && firstPv.score.value - nextPv.score.value > 100) {
+        picked = 0;
+        pv = firstPv;
+      } else {
+        picked = util.pickChance([3, 5, 7, 8, 8, 8, 10, 11, 15, 25]);
+        if (picked < 0 || picked > sortedPv.length) {
+          const chances = [];
+          for (let i = 0; i < sortedPv.length; i++) {
+            chances.push(100 / sortedPv.length);
+          }
+          picked = util.pickChance(chances);
+        }
+        pv = sortedPv[picked] || sortedPv[0];
+        result._bestmove = result.bestmove;
+        result.bestmove = pv.pv.split(" ")[0];
+        scores = sortedPv.slice(0, 10).map(x => x.score.value);
+      }
 
       console.log(
         id,
@@ -39,6 +59,8 @@ const makePvMove = async (engine, id) => {
         "scores",
         scores
       );
+    } else {
+      console.log(id, "no multi pv to try after sorted, using best move", result.bestmove);
     }
   } else {
     console.log(id, "no viable pv to try, using bestmove", result.bestmove);
@@ -47,7 +69,10 @@ const makePvMove = async (engine, id) => {
   return result;
 };
 
-const makeBestMove = async (engine, id, depth = 1) => {
+const makeBestMove = async (engine, id, depth) => {
+  if (!depth) {
+    depth = util.pickChance([10, 10, 20, 60], [4, 3, 2, 1]);
+  }
   console.log(id, "about to get move with depth", depth);
   const result = await engine.go({ depth });
   console.log(id, "make best move depth", depth, result.bestmove);
@@ -62,16 +87,14 @@ const engines = {
     },
     move: async engine => makePvMove(engine, "stockfish")
   },
-  stockfish1: {
-    name: "stockfish",
-    move: async engine => makeBestMove(engine, "stockfish1")
-  },
-  amyan: {
-    name: "amyan",
-    move: async engine => makeBestMove(engine, "amyan", 1)
+  komodo: {
+    name: "komodo",
+    initOptions: {
+      MultiPV: 10
+    },
+    move: async engine => makePvMove(engine, "komodo")
   }
 };
-
 module.exports = {
   name: "eeyore",
   firstName: "Eeyore",
@@ -113,6 +136,6 @@ module.exports = {
   engines,
 
   strategy: {
-    default: ["stockfish1", "stockfish", "amyan", "stockfish"]
+    default: ["stockfish"]
   }
 };
