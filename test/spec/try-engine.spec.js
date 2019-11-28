80dee5a9c2ba86f1d3ae3712ba87849b136fd1eb
Joel Chen <joel123@gmail.com>
2019-11-28 12:55:47 -0800

    add houdini engine

--- test/spec/try-engine.spec.js
+++ test/spec/try-engine.spec.js
@@ -5,16 +5,20 @@ const chessUtil = require("dgt-board/lib/utils");
 const chess = require("chess-js");
 
 describe("stockfish", function() {
-  let engine;
+  let engines = {};
   let play;
+
+  this.timeout(10000);
 
   before(async () => {
     const manager = new EngineMgr();
-    engine = await manager.initEngine({ name: "stockfish" });
+    engines.b = await manager.initEngine({ name: "stockfish", id: "b-sf" });
+    engines.w = await manager.initEngine({ name: "stockfish", id: "w-sf" });
   });
 
   after(async () => {
-    await engine.quit();
+    await engines.b.quit();
+    await engines.w.quit();
   });
 
   this.beforeEach(() => {
@@ -26,24 +30,34 @@ describe("stockfish", function() {
     console.log(chessUtil.rawToAscii(raw).join("\n"));
   }
 
+  function movePos(...moves) {
+    moves.forEach(move => {
+      play.move({
+        from: move.substr(0, 2),
+        to: move.substr(2)
+      });
+    });
+  }
+
   it("test 1", async () => {
     const fen1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     play.reset();
     play.load(fen1);
+    movePos("e2e4", "d7d5");
     const moves = [];
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 5; i++) {
       const fen = play.fen();
-      //   showFenAscii(fen);
+      const engine = engines[play.turn()];
       engine.position(fen);
-      const result = await engine.go({ depth: 10, multiPV: 10 });
+      const result = await engine.go({ depth: 20, multiPV: 10 });
       const bm = result.bestmove;
+      console.log(fen);
+      showFenAscii(fen);
+      console.log(bm);
       moves.push(bm);
-      play.move({
-        from: bm.substr(0, 2),
-        to: bm.substr(2)
-      });
+      movePos(bm);
     }
     console.log(moves);
     showFenAscii(play.fen());
-  });
+  }).timeout(100000);
 });
