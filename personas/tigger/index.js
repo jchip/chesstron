2ec274fafd2c79179509d8abac22fb9f1263310f
Joel Chen <joel123@gmail.com>
2019-11-29 20:07:19 -0800

    update tigger

--- personas/tigger/index.js
+++ personas/tigger/index.js
@@ -4,35 +4,34 @@ const _ = require("lodash");
 const util = require("../../lib/util");
 
 const makePvMove = async ({ engine, id, inDepth, game }) => {
-  const depth = 10; // inDepth || util.pickChance([5, 7, 8, 20, 30, 35], [7, 6, 5, 4, 3, 2]);
+  const depth = inDepth || util.pickChance([2, 4, 8, 86], [8, 6, 5, 4]);
   console.log(id, "about to make multi pv move, depth", depth);
   const result = await engine.go({
     depth,
-    MultiPV: 5
+    MultiPV: 10
   });
   if (result.info.length > 1) {
     const cmpScore = (a, b) => b.score.value - a.score.value;
     let sortedPv = util
       .simplifyPv(result.info)
-      // avoid moves that puts eeyore in significant disadvantage if possible
+      // avoid moves that puts tigger in significant disadvantage if possible
       .filter(x => x.pv && x.score && x.score.value > -250)
       .sort(cmpScore);
 
-    console.log(id, "bestmove", result.bestmove, "pv moves", result.info, "sorted", sortedPv);
+    console.log(id, "result", result, "sorted", sortedPv);
 
     if (sortedPv.length > 1) {
       let picked;
       let pickedMove;
 
-      const firstPv = sortedPv[0];
-
       if (sortedPv.find(x => x.score.unit === "mate")) {
+        const firstScore = sortedPv[0].score.value;
         sortedPv = sortedPv
           .map(x => {
             if (x.score.unit === "mate") {
               x.score = Object.assign({}, x, {
                 mate: x.score.value,
-                value: firstPv.score.value + x.score.value
+                value: firstScore + x.score.value
               });
             }
             return x;
@@ -40,46 +39,33 @@ const makePvMove = async ({ engine, id, inDepth, game }) => {
           .sort(cmpScore);
       }
 
-      const sortedPvByDest = util.pvByDest(sortedPv).sort(cmpScore);
-      const sortedPvBySrc = util.pvBySrc(sortedPv).sort(cmpScore);
-      console.log("sortedPvByDest", sortedPvByDest, "sortedPvBySrc", sortedPvBySrc);
-      const firstDiffByDest =
-        sortedPvByDest.length > 1
-          ? sortedPvByDest[0].score.value - sortedPvByDest[1].score.value
-          : 0;
-      const firstDiffBySrc =
-        sortedPvBySrc.length > 1 ? sortedPvBySrc[0].score.value - sortedPvBySrc[1].score.value : 0;
-      const firstDiff = firstPv.score.value - sortedPv[1].score.value;
-      // TODO: fix resume game lost history
+      const scores = util.balanceScores({ scores: sortedPv.slice(0, 25).map(x => x.score.value) });
       const moves = game ? game._chess.history().length : Infinity;
+      console.log("depth", depth, "scores", scores, "history moves", moves);
       // if our best move score is below 5, take best move
-      // or if first and second pv move has a diff bigger than 180, then
-      // opponent most likely made a big blunder, take obvious move 95% of the time
+      // or if first and second pv move has a diff bigger than 200, then
+      // opponent most likely made a big blunder, take obvious move
       // or if in first 6 moves and best move score is below 150, take best move
       if (
-        firstPv.score.value < 5 ||
-        (firstDiff > 180 && util.roll(95)) ||
-        (firstDiffByDest > 150 && util.roll(95)) ||
-        (firstDiffBySrc > 150 && util.roll(95)) ||
-        (firstPv.score.value < 150 && moves <= 6)
+        scores.length <= 1 ||
+        scores[0] < 5 ||
+        util.firstDiffCheck({ scores, threshold: 180, chance: 85 }) ||
+        (scores[0] < 150 && moves <= 6) ||
+        util.stdDevDiffCheck({
+          scores,
+          count: Math.floor(scores.length / 2) + 1,
+          threshold: 145,
+          chance: 90
+        })
       ) {
-        // TODO: if triggered by firstDiffByDest and there are multi moves
-        // for the dest square, do a random select for one of the moves
-        console.log(
-          "picking first pv move, firstDiff",
-          firstDiff,
-          "firstDiffByDest",
-          firstDiffByDest,
-          "firstDiffBySrc",
-          firstDiffBySrc
-        );
         picked = 0;
-        pickedMove = firstPv;
+        pickedMove = sortedPv[0];
+        console.log("picking first pv move", pickedMove, "bestmove", result.bestmove);
       } else {
         let playChances = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 8, 8, 15, 25];
-        if (sortedPv.length < playChances.length) {
-          const extraChances = playChances.slice(sortedPv.length).reverse();
-          playChances = playChances.slice(0, sortedPv.length);
+        if (scores.length < playChances.length) {
+          const extraChances = playChances.slice(scores.length).reverse();
+          playChances = playChances.slice(0, scores.length);
           extraChances.forEach((n, ix) => {
             const k = playChances.length - ix - 1;
             if (k >= 0) playChances[k] += n;
@@ -95,24 +81,12 @@ const makePvMove = async ({ engine, id, inDepth, game }) => {
         //   picked = util.pickChance(chances);
         // }
         pickedMove = sortedPv[picked] || sortedPv[0];
-        result._bestmove = result.bestmove;
-        result.bestmove = pickedMove.pv.split(" ")[0];
       }
 
-      console.log(
-        id,
-        "make pv picked",
-        picked,
-        "score",
-        pickedMove.score.value,
-        result.bestmove,
-        "depth",
-        depth,
-        "scores",
-        sortedPv.slice(0, 25).map(x => x.score.value),
-        "firstDiff",
-        firstDiff
-      );
+      result._bestmove = result.bestmove;
+      result.bestmove = pickedMove.san;
+
+      console.log(id, "make pv picked", picked, "score", pickedMove.score.value, result.bestmove);
     } else {
       console.log(id, "no multi pv to try after sorted, using best move", result.bestmove);
     }
